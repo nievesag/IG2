36a42500ea52987abc4fc6b251e057fcf1ef531b
nievesag <nievalon@ucm.es>
2025-11-27 23:56:34 +0100

    hmmm

--- Practica2/ProyectosOGREvc15x86/cambios/practica/sphereFS.glsl
+++ Practica2/ProyectosOGREvc15x86/cambios/practica/sphereFS.glsl
@@ -4,24 +4,39 @@ uniform sampler2D texName;
 uniform float flipping; // pregunta si se ha invertido el orden de los vertices (-1 invertido; 1 no invertido)
 
 in vec2 vUv0; 
+in vec3 vNormal;
+in vec3 vLightDirection;
+in vec3 vLightAmbient;
+in vec3 vLightDiffuse;
+in vec3 vMaterialDiffuse;
 
 out vec4 fFragColor;
 
 // calculos en el shader de fragmentos
 void main()
 {
-    vec4 color = texture(texName, vUv0); // color en ese fragmento
-    if (color.r > 0.6) { discard; } // si tiene mucho componente rojo -> discard
-
     bool frontFacing = (flipping > -1) ? gl_FrontFacing : !gl_FrontFacing; // si el fragmento es de la cara frontal
 
+    vec3 color = vec3(texture(texName, vUv0)); // color en ese fragmento
+    if (color.r > 0.6) { discard; } // si tiene mucho componente rojo -> discard
+
+    vec3 ambient = vLightAmbient*vMaterialDiffuse;
+    vec3 diffuse;
+    
     if(frontFacing)
     {
-        fFragColor = texture(texName, vUv0);
+        diffuse = max(0, dot(vNormal, -vLightDirection))*vLightDiffuse*vMaterialDiffuse;
+        //fFragColor = texture(texName, vUv0);
     }
     else
     {
+        color = -vNormal;
+        diffuse = max(0, dot(-vNormal, -vLightDirection))*vLightDiffuse*vMaterialDiffuse;
+
         // !!! falta gestion de mapa de normales en el interior de la esfera
-        fFragColor = vec4(0.0, 0.0, 1.0, 1.0); // lo pone azul de momento por poner algo
+        //fFragColor = vec4(0.0, 0.0, 1.0, 1.0); // lo pone naranja de momento por poner algo
     }
+
+    color = color * (ambient + diffuse);
+    fFragColor = vec4(color,1.0);
 }
--- Practica2/ProyectosOGREvc15x86/cambios/practica/sphereVS.glsl
+++ Practica2/ProyectosOGREvc15x86/cambios/practica/sphereVS.glsl
@@ -1,14 +1,29 @@
 #version 330 core
 
 uniform mat4 modelViewProjMat;
+uniform vec3 lightDirection;
+uniform vec3 lightDiffuse;
+uniform vec3 lightAmbient;
+uniform vec3 materialDiffuse;
 
 in vec4 vertex; 
 in vec2 uv0;
+in vec3 normal;
 
 out vec2 vUv0;
+out vec3 vNormal;
+out vec3 vLightDirection;
+out vec3 vLightDiffuse;
+out vec3 vLightAmbient;
+out vec3 vMaterialDiffuse;
 
 void main()
 {
     vUv0 = uv0; 
+    vNormal = normal;
+    vLightDirection = lightDirection;
+    vLightDiffuse = lightDiffuse;
+    vLightAmbient = lightAmbient;
+    vMaterialDiffuse = materialDiffuse;
     gl_Position = modelViewProjMat * vertex;
 }
